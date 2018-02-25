36d0a42a47f3155792cd5d224680078aca106a3c
Ckend <83493903@qq.com>
2018-02-25 16:40:22 +0800

    1.0

---
+++ Sync_WeiBo.php
@@ -0,0 +1,120 @@
+<?php
+/*
+Plugin Name: 微博自动同步助手
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: 发表文章时自动同步到微博，内容包括简介和文章中第一个图片，支持自定义微博模板。
+Version: 1.0
+Author: Alltoshare
+Author URI: https://alltoshare.com
+E-mail: ckend@alltoshare.com
+License: GPL
+*/
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+define( 'Sync_Weibo_PATH', plugin_dir_path( __FILE__ ) );
+
+include_once( Sync_Weibo_PATH . "class-weibo.php" );
+add_action('save_post', 'sync_to_sina_weibo', 10, 1);
+
+add_action('admin_menu', 'Sync_Weibopage');
+function Sync_Weibopage() {
+  //call register settings function
+	add_action( 'admin_init', 'register_Sync_Weibosettings' );
+  add_options_page('微博自动同步助手','微博自动同步助手', 'manage_options', 'Sync_Weibo', 'Sync_Weibosettings_page' );
+}
+
+function register_Sync_Weibosettings() {
+	//register our settings
+	register_setting( 'sync-weibo-settings', 'appkey' );
+  register_setting( 'sync-weibo-settings', 'name' );
+  register_setting( 'sync-weibo-settings', 'password' );
+  register_setting( 'sync-weibo-settings', 'content' );
+  register_setting( 'sync-weibo-settings', 'withPicture' );
+}
+
+function Sync_Weibosettings_page() {
+    if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+?>
+<div class="wrap">
+<h2>同步新浪微博配置(Sync Weibo Options)</h2><br />
+<div id="poststuff" class="metabox-holder has-right-sidebar">
+  <div class="inner-sidebar">
+    <div style="position:absolute;" class="meta-box-sortabless ui-sortable" id="side-sortables">
+      <div class="postbox" id="sm_pnres">
+            <h3 class="hndle"><span>捐赠</span></h3>
+            <div class="inside" style="margin:0;padding-top:10px;background-color:#ffffe0;">
+                <p>
+                  本插件由<a href="https://alltoshare.com">极致分享</a>开发和维护，如果你想支持开发者，可以进行捐助。非常感谢您的支持!
+                </p>
+                  <br />
+                  <img src="<?php echo esc_url( 'https://alltoshare.com/wp-content/uploads/2018/02/2018_02_25_1542307269.png' ); ?>" alt="微信支付" width="250"/>
+            </div>
+        </div>
+    </div>
+  </div>
+	<div class="has-sidebar-content" id="post-body-content">
+    <p>若您不需要使用自定义微博模板，将其置空即可使用默认模板。</p>
+    <p>模板支持以下元字符：</p>
+    <p>%title%: 提取标题</p>
+    <p>%excerpt begin=0 len=120%: 提取摘要。将取从begin开始的len个长度的字符。</p>
+    <p>%content begin=0 len=120%: 提取内容。 注意事项同上。</p>
+    <p>%link%: 提取文章链接。</p>
+    <p>模板中以上字符仅允许出现一次，另外%link%必须出现一次，此外len不应该超过120。</p>
+    <p>缺少%link%, 或者文字长度超出微博允许范围将可能同步失败。</p>
+    <p>示例： %title%: %excerpt begin=0 len=120% ... 查看全文:%link%</p>
+		<form method="post" action="options.php">
+		  <?php settings_fields( 'sync-weibo-settings' ); ?>
+			<table class="form-table">
+				<tr valign="top">
+				</tr>
+				<tr valign="top">
+				<th scope="row">新浪微博账号(Sina Weibo name) </th>
+				<td>
+					<input name="name" type="text" id="name" value="<?php echo get_option('name'); ?>" class="regular-text" />
+				</td>
+				</tr>
+				<tr valign="top">
+				<th scope="row">新浪微博密码(Sina Weibo password)</th>
+				<td>
+					<input name="password" type="password" id="password" value="<?php echo get_option('password'); ?>" class="regular-text" />
+				</td>
+				</tr>
+				<tr valign="top">
+				<th scope="row">新浪微博appkey(Sina Weibo appkey)</th>
+				<td>
+					<input name="appkey" type="text" id="appkey" value="<?php echo get_option('appkey'); ?>" class="regular-text" />
+				</td>
+				</tr>
+        <tr valign="top">
+				<th scope="row">自定义微博模板</th>
+				<td>
+					<input name="content" type="text" id="content" value="<?php echo get_option('content'); ?>" class="regular-text" />
+				</td>
+        </tr>
+			</table>
+      <th scope="row">是否带图片     </th>
+      <td>
+        <input name="withPicture" type="radio" id="withPicture_no" value="0"<?php checked(0, get_option('withPicture')); ?> />
+        <label class="description">否</label>
+        <input name="withPicture" type="radio" id="withPicture_yes" value="1"<?php checked(1, get_option('withPicture')); ?> />
+        <label class="description">是</label>
+      </td>
+		  <p class="submit">
+			<input type="submit" class="button-primary" value="<?php  _e('Save Changes') ?>" />
+			</p>
+		</form>
+	</div>
+</div>
+</div>
+
+
+
+<?php
+}
+
+?>
---
+++ class-weibo.php
@@ -0,0 +1,92 @@
+<?php
+/**
+* WordPress 发表文章时同步到新浪微博
+* WordPress sync to Sina weibo when published a post;
+* Code from https://alltoshare.com
+*/
+function sync_to_sina_weibo($post_ID) {
+  if( wp_is_post_revision($post_ID) ) return;
+  $appkey = get_option('appkey');
+  $username = get_option('name');
+  $userpassword = get_option('password');
+  $user_content = get_option('content');
+  $withPicture = get_option('withPicture');
+  echo $withPicture;
+  $get_post_info = get_post($post_ID);
+  $get_post_excerpt = get_post($post_ID)->post_excerpt;
+  $get_post_content = get_post($post_ID)->post_excerpt;
+  $get_post_img = get_post($post_ID)->post_content;
+	preg_match_all('/<img.*?(?: |\\t|\\r|\\n)?src=[\'"]?(.+?)[\'"]?(?:(?: |\\t|\\r|\\n)+.*?)?>/sim', $get_post_img, $strResult, PREG_PATTERN_ORDER);
+
+  if(count($strResult[1]) > 0 && $withPicture == 1)
+      $imgUrl = $strResult[1][0];//获得第一张图url地址
+  else
+      $imgUrl = null;
+	echo $imgUrl;
+
+  //去掉文章内的html编码的空格、换行、tab等符号
+  $get_post_excerpt = str_replace("\t", " ", str_replace("\n", " ", str_replace("&nbsp;", " ", $get_post_excerpt)));
+  $get_post_content = str_replace("\t", " ", str_replace("\n", " ", str_replace("&nbsp;", " ", $get_post_content)));
+
+  //获取文章标题
+  $get_post_title = get_post($post_ID)->post_title;
+  $user_content = '%title%: %excerpt begin=0 len=120% ... 查看全文:%link%';
+  if ( $get_post_info->post_status == 'publish' && $_POST['original_post_status'] != 'publish' ) {
+	  $request = new WP_Http;
+    if ($user_content != '') {
+      // 微博文字
+      $user_content = str_replace('%title%',strip_tags( $get_post_title ),$user_content);
+
+      if(strstr($user_content,'%content ')!=FALSE){
+        preg_match_all('/%content begin=\d* len=[1-9]\d*%/', $user_content, $user_content_content, PREG_PATTERN_ORDER);
+        preg_match_all('/[0-9]\d*/', $user_content_content[0][0], $user_content_content_len, PREG_PATTERN_ORDER);
+        $user_content = str_replace($user_content_content[0][0],mb_strimwidth(strip_tags( apply_filters('the_content', $get_post_content)),$user_content_content_len[0][0], $user_content_content_len[0][1]),$user_content);
+      }
+      if(strstr($user_content, '%excerpt ')!=FALSE){
+        preg_match_all('/%excerpt begin=\d* len=[1-9]\d*%/', $user_content, $user_content_content, PREG_PATTERN_ORDER);
+        preg_match_all('/[0-9]\d*/', $user_content_content[0][0], $user_content_content_len, PREG_PATTERN_ORDER);
+        $user_content = str_replace($user_content_content[0][0],mb_strimwidth(strip_tags( apply_filters('the_content', $get_post_excerpt)),$user_content_content_len[0][0], $user_content_content_len[0][1]),$user_content);
+      }
+      $user_content = str_replace('%link%',get_permalink($post_ID),$user_content);
+
+      echo $user_content;
+      $status = $user_content;
+    }else{
+  	  $status = '【' . strip_tags( $get_post_title ) . '】 ' . mb_strimwidth(strip_tags( apply_filters('the_content', $get_post_excerpt)),0, 132,'...') . ' 全文地址:' . get_permalink($post_ID) ;
+    }
+	  $body = array( 'status' => $status, 'source'=> $appkey);
+	  $headers = array('Authorization' => 'Basic ' . base64_encode("$username:$userpassword"));
+    $api_url = 'https://api.weibo.com/2/statuses/share.json';
+    if($imgUrl!==null)
+	  {
+	    $body['pic'] = $imgUrl;
+	    uksort($body, 'strcmp');
+	      $str_b=uniqid('------------------');
+	      $str_m='--'.$str_b;
+	      $str_e=$str_m. '--';
+	      $tmpbody='';
+	      foreach($body as $k=>$v){
+	          if($k=='pic'){
+	              $img_c=file_get_contents($imgUrl);
+	              $url_a=explode('?', basename($imgUrl));
+	              $img_n=$url_a[0];
+	              $tmpbody.=$str_m."\r\n";
+	              $tmpbody.='Content-Disposition: form-data; name="'.$k.'"; filename="'.$img_n.'"'."\r\n";
+	              $tmpbody.="Content-Type: image/unknown\r\n\r\n";
+	              $tmpbody.=$img_c."\r\n";
+	          }else{
+	              $tmpbody.=$str_m."\r\n";
+	              $tmpbody.='Content-Disposition: form-data; name="'.$k."\"\r\n\r\n";
+	              $tmpbody.=$v."\r\n";
+	          }
+	      }
+	      $tmpbody.=$str_e;
+	      $body = $tmpbody;
+	    	$headers['Content-Type'] = 'multipart/form-data; boundary='.$str_b;
+	    }
+      $result = $request->post( $api_url , array( 'body' => $body, 'headers' => $headers ) );
+			echo json_encode($result);
+			return json_encode($result);
+	}
+}
+?>
